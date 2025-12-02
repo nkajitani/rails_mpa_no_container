26105eacc782f9a39b3f61c08735316e8f90284f
nkajitani <kajitani719@gmail.com>
2025-12-02 11:40:36 +0900

    include vite-tsconfig-paths in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    tsconfigPaths(),
   ],
 })
