7215425e8401ee7841608d4c28da9fc47cf66b7a
nkajitani <kajitani719@gmail.com>
2025-12-16 18:29:18 +0900

    reflash page when turbo frame miss

--- app/frontend/javascript/index/admin.ts
+++ app/frontend/javascript/index/admin.ts
@@ -1,5 +1,16 @@
 import application from "@/javascript/application";
 import flatpickr_controller from "@/javascript/controllers/flatpickr_controller";
-
+import type { TurboFrameMissingEvent } from "@hotwired/turbo";
 
 application.register("flatpickr", flatpickr_controller);
+
+document.addEventListener("turbo:frame-missing",
+  async (event: TurboFrameMissingEvent) => {
+    const {
+      detail: { response, visit },
+    } = event;
+
+    event.preventDefault();
+    await visit(response, { action: "replace" });
+  }
+);
